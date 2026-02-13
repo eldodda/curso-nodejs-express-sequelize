1b7f0dd22f1053ff12e9b87bf10e2ae855f6669c
Douglas Lima <dugbits@gmail.com>
2026-02-13 10:26:58 -0300

    Continuidade na API

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -3,9 +3,21 @@ const PessoaServices = require('../services/PessoaServices.js');
 
 const pessoaServices = new PessoaServices();
 
-class PessoaController extends Controller{
-  constructor(){
+class PessoaController extends Controller {
+  constructor() {
     super(pessoaServices);
+  }
+
+  async pegaMatriculas(req, res) {
+    const { estudanteId } = req.params;
+    try {
+      const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+      return res
+        .status(200)
+        .json(listaMatriculas);
+    } catch (erro) {
+      //erro
+    }
   }
 }
 
--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -5,9 +5,12 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
     static associate(models) {
-      Pessoa.hasMany(models.Curso, {foreignKey: 'docente_id'
+      Pessoa.hasMany(models.Curso, {
+        foreignKey: 'docente_id'
       });
-      Pessoa.hasMany(models.Matricula, {foreignKey: 'estudante_id'
+      Pessoa.hasMany(models.Matricula, {
+        foreignKey: 'estudante_id',
+        as: 'aulasMatriculadas'
       });
     }
   }
